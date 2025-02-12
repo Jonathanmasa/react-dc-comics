14953166e1c793c4ac80d7f4bea68dac8beda7cc
Jonathanmasa <jonathanmasa97@gmail.com>
2025-02-12 14:59:46 +0100

    feat[initial-vite-img-components]

---
+++ src/components/Header.jsx
@@ -0,0 +1,4 @@
+// Header.jsx
+export default function Header() {
+    return (<header> /* contenuto qui */ </header>);
+    }
---
+++ src/components/Main.jsx
@@ -0,0 +1,4 @@
+// Main.jsx
+export default function Main() {
+    return (<main> /* contenuto qui */ </main>);
+    }
