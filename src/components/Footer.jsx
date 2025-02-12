52957a0adcad88b5f599e4f6c826ef4f350701f6
Jonathanmasa <jonathanmasa97@gmail.com>
2025-02-12 19:08:06 +0100

    draft[footer-jumbotron]

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,11 +2,10 @@
 export default function Footer() {
     return (
     <footer>
-         <div className="container p-3 foot-jumbotron">
-            <div className="row foot-jumbotron">
-                <div className="col-md-3">
+         <div className="container-fluid pt-3 foot-jumbotron d-flex">
+                <div className="jumbo-width">
                     <div className="row">
-                        <div className="col-md-6">
+                        <div className="col-md-2">
                             <h5>DC COMICS</h5>
                             <ul className="list-unstyled">
                                 <li><a href="#">Characters</a></li>
@@ -23,7 +22,7 @@ export default function Footer() {
                                 <li><a href="#">Shop DC Collectibles</a></li>
                             </ul>
                         </div>
-                        <div className="col-md-6">
+                        <div className="col-md-2">
                             <h5>DC</h5>
                             <ul className="list-unstyled">
                                 <li><a href="#">Terms Of Use</a></li>
@@ -35,32 +34,32 @@ export default function Footer() {
                                 <li><a href="#">Talent Workshops</a></li>
                             </ul>
                         </div>
+                        <div className="col-md-8">
+                            <h5>SITES</h5>
+                            <ul className="list-unstyled">
+                                <li><a href="#">DC</a></li>
+                                <li><a href="#">MAD Magazine</a></li>
+                                <li><a href="#">DC Kids</a></li>
+                                <li><a href="#">DC Universe</a></li>
+                                <li><a href="#">DC Power Visa</a></li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
-                <div className="col-md-6">
-                    <h5>SITES</h5>
-                    <ul className="list-unstyled">
-                        <li><a href="#">DC</a></li>
-                        <li><a href="#">MAD Magazine</a></li>
-                        <li><a href="#">DC Kids</a></li>
-                        <li><a href="#">DC Universe</a></li>
-                        <li><a href="#">DC Power Visa</a></li>
-                    </ul>
-                </div>
+                <img src="/public/dc-logo-bg.png" alt="dc logo" />
             </div>
-        </div>
         <div className="footer-bar">
                 <div className="last-footer d-flex justify-content-between pb-2">
                     <div className="text-center mt-3">
                         <button className="btn btn-outline-primary">SIGN-UP NOW!</button>
                     </div>
-                    <div class="text-center mt-3 social-icons">
+                    <div class="mt-3 social-icons">
                         <h5>FOLLOW US</h5>
-                        <a href="#"><i className="fab fa-facebook-f"></i></a>
-                        <a href="#"><i className="fab fa-twitter"></i></a>
-                        <a href="#"><i className="fab fa-youtube"></i></a>
-                        <a href="#"><i className="fab fa-pinterest"></i></a>
-                        <a href="#"><i className="fab fa-instagram"></i></a>
+                        <a href="#"><img className="fab fa-facebook" src="/public/footer-facebook.png" alt="facebook"/></a>
+                        <a href="#"><img className="fab fa-twitter" src="/public/footer-twitter.png" alt="facebook"/></a>
+                        <a href="#"><img className="fab fa-youtube" src="/public/footer-youtube.png" alt="facebook"/></a>
+                        <a href="#"><img className="fab fa-pinterst" src="/public/footer-pinterest.png" alt="facebook"/></a>
+                        <a href="#"><img className="fab fa-google" src="/public/footer-periscope.png" alt="facebook"/></a>
                     </div>
                 </div>
         </div>
