060e9dbb6e66df5f133f9a949bd118008a571770
Jonathanmasa <jonathanmasa97@gmail.com>
2025-02-13 16:24:20 +0100

    feat[props-navbar]

--- src/App.jsx
+++ src/App.jsx
@@ -12,9 +12,22 @@ import Main from "./components/Main.jsx";
 import Footer from "./components/Footer.jsx";
 
 function App() {
+  // definisco l'array dei links e lo passo come props a header.jsx
+  const navbarLinks = [
+    { id: 1, text: "Characters", url: "#", current: false },
+    { id: 2, text: "Comics", url: "#", current: true },
+    { id: 3, text: "Movies", url: "#", current: false },
+    { id: 4, text: "TV", url: "#", current: false },
+    { id: 5, text: "Games", url: "#", current: false },
+    { id: 6, text: "Collectables", url: "#", current: false },
+    { id: 7, text: "Videos", url: "#", current: false },
+    { id: 8, text: "Fans", url: "#", current: false },
+    { id: 9, text: "Views", url: "#", current: false },
+    { id: 10, text: "Shop", url: "#", current: false },
+  ];
   return (
     <>
-      <Header />
+      <Header links= {navbarLinks} />
       <Main />
       <Footer />
     </>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,43 +1,40 @@
 // Header.jsx
-export default function Header() {
-    // dati ricavati per la gestione dei link navbar
-    const links = [
-        { id: 1, text: 'Characters', url: '#', current: false },
-        { id: 2, text: 'Comics', url: '#', current: true },
-        { id: 3, text: 'Movies', url: '#', current: false },
-        { id: 4, text: 'TV', url: '#', current: false },
-        { id: 5, text: 'Games', url: '#', current: false },
-        { id: 6, text: 'Collectionables', url: '#', current: false },
-        { id: 7, text: 'Videos', url: '#', current: false },
-        { id: 8, text: 'Fans', url: '#', current: false },
-        { id: 9, text: 'Views', url: '#', current: false },
-        { id: 10, text: 'Shop', url: '#', current: false },
-    ];
-    
+export default function Header({links}) {
     return (
     <header>
          <nav className="navbar navbar-expand-lg navbar-light navbar-header">
             <div className="container">
-                <a className="navbar-brand" href="#"><img src="/public/dc-logo.png" alt="DC Logo"/></a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+
+                <a className="navbar-brand" href="#">
+                    <img src="/public/dc-logo.png" alt="DC Logo" />
+                </a>
+                <button
+                    className="navbar-toggler"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#navbarNav"
+                >
                     <span className="navbar-toggler-icon"></span>
                 </button>
+                
+                {/* Contenitore della navbar con i link */}
                 <div className="collapse navbar-collapse" id="navbarNav">
                     <ul className="navbar-nav ms-auto">
-                        <li className="nav-item"><a className="nav-link" href="#">Characters</a></li>
-                        <li className="nav-item   nav-active"><a className="nav-link" href="#">Comics</a></li>
-                        <li className="nav-item"><a className="nav-link" href="#">Movies</a></li>
-                        <li className="nav-item"><a className="nav-link" href="#">TV</a></li>
-                        <li className="nav-item"><a className="nav-link" href="#">Games</a></li>
-                        <li className="nav-item"><a className="nav-link" href="#">Collectionables</a></li>
-                        <li className="nav-item"><a className="nav-link" href="#">Videos</a></li>
-                        <li className="nav-item"><a className="nav-link" href="#">Fans</a></li>
-                        <li className="nav-item"><a className="nav-link" href="#">Views</a></li>
-                        <li className="nav-item"><a className="nav-link" href="#">Shop</a></li>
+                        {links.map((link) => (
+                            <li key={link.id} className="nav-item">
+                                <a
+                                    className={`nav-link ${link.current ? "active" : ""}`}
+                                    href={link.url}
+                                >
+                                    {link.text}
+                                </a>
+                            </li>
+                        ))}
                     </ul>
-                </div>
+                 </div>
+
             </div>
-        </nav>
-  
-    </header>);
-    }
+      </nav>
+  </header>
+ );
+}
