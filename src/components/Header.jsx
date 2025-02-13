697e59a62751746977a13afbd20bf7429b06c671
Jonathanmasa <jonathanmasa97@gmail.com>
2025-02-13 15:28:48 +0100

    feat[array-links-navbar]

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,19 @@
 // Header.jsx
 export default function Header() {
+    // dati ricavati per la gestione dei link navbar
+    const links = [
+        { id: 1, text: 'Characters', url: '#', current: false },
+        { id: 2, text: 'Comics', url: '#', current: true },
+        { id: 3, text: 'Movies', url: '#', current: false },
+        { id: 4, text: 'TV', url: '#', current: false },
+        { id: 5, text: 'Games', url: '#', current: false },
+        { id: 6, text: 'Collectionables', url: '#', current: false },
+        { id: 7, text: 'Videos', url: '#', current: false },
+        { id: 8, text: 'Fans', url: '#', current: false },
+        { id: 9, text: 'Views', url: '#', current: false },
+        { id: 10, text: 'Shop', url: '#', current: false },
+    ];
+    
     return (
     <header>
          <nav className="navbar navbar-expand-lg navbar-light navbar-header">
